0752581da84a1d72a640522f8b44df67373bfe94
ritesh81269@gmail.com <ritesh81269@gmail.com>
2023-10-01 22:29:27 +0530

    fixing cookie issue

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,7 +5,8 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'None',
     }
 
     res.status(statusCode).cookie('token', token, options).json({
