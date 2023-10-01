4efd017b9c40dbfaf30e2be26761a2df70588935
ritesh81269@gmail.com <ritesh81269@gmail.com>
2023-10-01 20:50:39 +0530

    updated server address

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://flipkart-clone-ochre-three.vercel.app",
     withCredentials:true
 })
 
